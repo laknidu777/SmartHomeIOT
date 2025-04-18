350e0358837d4fcfc9527c337585ec8cd5abc17c
Laknidu Thilakawardhana <10899430@students.plymouth.ac.uk>
2025-04-18 22:57:40 +0530

    deleted old backend

--- Smarthome-Backend/routes/categoryRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { createCategory, getCategories } = require("../controllers/categoryController");
-const authenticate = require("../middleware/authMiddleware");
-
-router.post("/", authenticate, createCategory);
-router.get("/", authenticate, getCategories);
-
-module.exports = router;
--- Smarthome-Backend/routes/deviceRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getDeviceStatus,updateDeviceStatus,pairDevice } = require("../controllers/deviceController");
-
-router.post("/pair", pairDevice);
-router.get("/:espId", getDeviceStatus);
-router.put("/:espId", updateDeviceStatus);
-module.exports = router;
